f31f348192313804bec29317d503c5af9d34776e
Alejandro Ramirez <8104677+jebuschrast@users.noreply.github.com>
2024-05-19 14:33:30 -0700

    shell plugin fix

--- scripts/core/install_shell_plugins.sh
+++ scripts/core/install_shell_plugins.sh
@@ -10,7 +10,7 @@ clone_zsh_plugins() {
     # Clone zsh-autosuggestions
     if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
         echo "Installing zsh-autosuggestions..."
-        git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+        git clone https://github.com/zsh-users/zsh-autosuggestions  ${ZSH_CUSTOM)/plugins/zsh-autosuggestions
     else
         echo "zsh-autosuggestions is already installed."
     fi
@@ -18,7 +18,7 @@ clone_zsh_plugins() {
     # Clone zsh-syntax-highlighting
     if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
         echo "Installing zsh-syntax-highlighting..."
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM)/plugins/zsh-syntax-highlighting
     else
         echo "zsh-syntax-highlighting is already installed."
     fi
@@ -28,7 +28,7 @@ clone_zsh_plugins() {
     # clone you-should-use
     if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/you-should-use" ]; then
         echo "Installing you-should-use..."
-        git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.oh-my-zsh/custom/pplugins/you-should-use
+        git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM)/plugins/you-should-use
     else
         echo "you-should-use is already installed."
     fi
@@ -43,6 +43,11 @@ clone_zsh_plugins() {
     
 }
 
+# check if zsh is installed
+if [ -z "$(command -v zsh)" ]; then
+    echo "Zsh is not installed. Please install Zsh first."
+    exit 1
+fi
 # Check if Oh My Zsh is installed
 if [ -d "$HOME/.oh-my-zsh" ]; then
     clone_zsh_plugins
